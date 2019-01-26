c257d1ee9501c3d66d1d622b2fd035fad16db9de
Eugene Lin <me@eugenel.in>
2019-01-26 18:21:49 +0000

    Updated config

--- web/config.php
+++ web/config.php
@@ -6,4 +6,6 @@
   $db_name = "educate";
   $db = mysqli_connect($db_hostname, $db_username, $db_password, $db_name) or die("{\"status\":0,\"content\":\"Failed to connect to database\"}");
 
+  $google_client = "258775497785-p455232de9ot6asva9dh9k1o03mte93f.apps.googleusercontent.com";
+  $google_secret = $sec_google_secret;
 ?>
